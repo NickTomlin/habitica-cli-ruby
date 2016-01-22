be38a9422cd9138f7aeeb317664a68a46551e188
Nick Tomlin <nick.tomlin@gmail.com>
2016-01-22 16:33:30 -0600

    Allow overriding habit user and key via flags

--- lib/habitica_cli/runner.rb
+++ lib/habitica_cli/runner.rb
@@ -7,6 +7,9 @@ require 'habitica_cli/tasks/habit'
 module HabiticaCli
   # encapsulate our sub commands
   class Runner < Thor
+    class_option :'habit_user', :default => ENV['HABIT_USER']
+    class_option :'habit_key', :default => ENV['HABIT_KEY']
+
     desc 'daily COMMAND ...ARGS', 'list or update dailies'
     subcommand 'daily', Daily
     desc 'todo COMMAND ...ARGS', 'list or update todos'
--- lib/habitica_cli/task.rb
+++ lib/habitica_cli/task.rb
@@ -14,7 +14,12 @@ module HabiticaCli
     end
 
     def api
-      Api.new(ENV['HABIT_USER'], ENV['HABIT_KEY'])
+      user = parent_options['habit_user']
+      key = parent_options['habit_key']
+      if user.empty? || key.empty?
+        raise "You must provide a habit user and api key \n\n do this via (HABIT_USER and HABIT_KEY) or the --habit_user --habit_key"
+      end
+      Api.new(user, key)
     end
   end
 end
